0bc4ae26db532062744967f325567b1fbbd0c4fd
NET Admin <ms@sempek.cf>
2018-11-01 11:41:53 +0100

    Pierwszy program Hello. Tablica args.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello
 {
@@ -10,12 +6,14 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            string name = args[0];
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Witaj " + name);
+
+            //foreach (var item in args)
+            //{
+            //    Console.WriteLine("witaj " + item);
+            //}
         }
     }
 }
