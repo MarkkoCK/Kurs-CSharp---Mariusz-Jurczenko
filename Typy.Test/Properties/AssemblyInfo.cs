90e3dc714af54de5658b94d8c7bd8e8e71b8350b
NET Admin <ms@sempek.cf>
2018-12-26 18:57:39 +0100

    Aktualizacja

---
+++ Typy.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Typy.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Typy.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ac8b52e-c27c-499b-a856-c496dd99a7ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Typy.Test/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Typy.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateMaxGrade()
+        {
+            Diary diary = new Diary();
+            diary.AddRating(8.9f);
+            diary.AddRating(8f);
+            diary.AddRating(7f);
+            diary.AddRating(5f);
+            diary.AddRating(3f);
+            diary.AddRating(10f);
+
+            DiaryStatistics stats = diary.ComputeStatistics();
+
+            Assert.AreEqual(10f, stats.MaxGrade);        
+        }
+
+        [TestMethod]
+        public void PassByValueClass()
+        {
+            Diary diary = new Diary();
+            diary.Name = "Jacek";
+
+            SetName(diary);
+
+            Assert.AreEqual("Marcin", diary.Name);
+        }
+
+        private void SetName(Diary diary)
+        {
+            diary.Name = "Marcin";
+        }
+
+        [TestMethod]
+        public void TestIsTrue()
+        {
+            bool value = true;
+            Assert.IsTrue(value);
+        }
+    }
+}
